04f173d1f7f86e53af46befa8ae30c3d9210f253
decanus <7621705+decanus@users.noreply.github.com>
2023-09-26 13:34:32 +0200

    fix

--- tick_math.go
+++ tick_math.go
@@ -2,10 +2,11 @@ package uniswap_v3_simulator
 
 import (
 	"errors"
-	"github.com/daoleno/uniswapv3-sdk/constants"
-	"github.com/shopspring/decimal"
 	"math"
 	"math/big"
+
+	"github.com/daoleno/uniswapv3-sdk/constants"
+	"github.com/shopspring/decimal"
 )
 
 var POWERS_OF_2 []struct {
@@ -88,16 +89,36 @@ func GetTickAtSqrtRatio(sqrtRatioX96D decimal.Decimal) (int, error) {
 	}
 }
 
-func mulShift(val decimal.Decimal, mulBy string) decimal.Decimal {
-	byBi, _ := big.NewInt(0).SetString(mulBy, 16)
-	by := decimal.NewFromBigInt(byBi, 0)
-	tmp := val.Mul(by).BigInt()
+func mulShift(val decimal.Decimal, by *big.Int) decimal.Decimal {
+	tmp := new(big.Int).Mul(val.BigInt(), by)
 	tmp = tmp.Rsh(tmp, 128)
 	return decimal.NewFromBigInt(tmp, 0)
 }
 
 var (
 	INVALID_TICK = errors.New("invalid tick")
+)
+
+var (
+	mulShiftBy2, _     = new(big.Int).SetString("fff97272373d413259a46990580e213a", 16)
+	mulShiftBy4, _     = new(big.Int).SetString("fff2e50f5f656932ef12357cf3c7fdcc", 16)
+	mulShiftBy8, _     = new(big.Int).SetString("ffe5caca7e10e4e61c3624eaa0941cd0", 16)
+	mulShiftBy10, _    = new(big.Int).SetString("ffcb9843d60f6159c9db58835c926644", 16)
+	mulShiftBy20, _    = new(big.Int).SetString("ff973b41fa98c081472e6896dfb254c0", 16)
+	mulShiftBy40, _    = new(big.Int).SetString("ff2ea16466c96a3843ec78b326b52861", 16)
+	mulShiftBy80, _    = new(big.Int).SetString("fe5dee046a99a2a811c461f1969c3053", 16)
+	mulShiftBy100, _   = new(big.Int).SetString("fcbe86c7900a88aedcffc83b479aa3a4", 16)
+	mulShiftBy200, _   = new(big.Int).SetString("f987a7253ac413176f2b074cf7815e54", 16)
+	mulShiftBy400, _   = new(big.Int).SetString("f3392b0822b70005940c7a398e4b70f3", 16)
+	mulShiftBy800, _   = new(big.Int).SetString("e7159475a2c29b7443b29c7fa6e889d9", 16)
+	mulShiftBy1000, _  = new(big.Int).SetString("d097f3bdfd2022b8845ad8f792aa5825", 16)
+	mulShiftBy2000, _  = new(big.Int).SetString("a9f746462d870fdf8a65dc1f90e061e5", 16)
+	mulShiftBy4000, _  = new(big.Int).SetString("70d869a156d2a1b890bb3df62baf32f7", 16)
+	mulShiftBy8000, _  = new(big.Int).SetString("31be135f97d08fd981231505542fcfa6", 16)
+	mulShiftBy10000, _ = new(big.Int).SetString("9aa508b5b7a84e1c677de54f3e99bc9", 16)
+	mulShiftBy20000, _ = new(big.Int).SetString("5d6af8dedb81196699c329225ee604", 16)
+	mulShiftBy40000, _ = new(big.Int).SetString("2216e584f5fa1ea926041bedfe98", 16)
+	mulShiftBy80000, _ = new(big.Int).SetString("48a170391f7dc42444e8fa2", 16)
 )
 
 func GetSqrtRatioAtTick(tick int) (decimal.Decimal, error) {
@@ -115,84 +136,70 @@ func GetSqrtRatioAtTick(tick int) (decimal.Decimal, error) {
 	ratio = decimal.NewFromBigInt(ratioBi, 0)
 
 	if (absTick & 0x2) != 0 {
-		ratio = mulShift(ratio, "fff97272373d413259a46990580e213a")
+		ratio = mulShift(ratio, mulShiftBy2)
 	}
 	if (absTick & 0x4) != 0 {
-
-		ratio = mulShift(ratio, "fff2e50f5f656932ef12357cf3c7fdcc")
+		ratio = mulShift(ratio, mulShiftBy4)
 	}
 	if (absTick & 0x8) != 0 {
-
-		ratio = mulShift(ratio, "ffe5caca7e10e4e61c3624eaa0941cd0")
+		ratio = mulShift(ratio, mulShiftBy8)
 	}
 	if (absTick & 0x10) != 0 {
-
-		ratio = mulShift(ratio, "ffcb9843d60f6159c9db58835c926644")
+		ratio = mulShift(ratio, mulShiftBy10)
 	}
 	if (absTick & 0x20) != 0 {
-		ratio = mulShift(ratio, "ff973b41fa98c081472e6896dfb254c0")
-
+		ratio = mulShift(ratio, mulShiftBy20)
 	}
 	if (absTick & 0x40) != 0 {
-
-		ratio = mulShift(ratio, "ff2ea16466c96a3843ec78b326b52861")
+		ratio = mulShift(ratio, mulShiftBy40)
 	}
 	if (absTick & 0x80) != 0 {
-
-		ratio = mulShift(ratio, "fe5dee046a99a2a811c461f1969c3053")
+		ratio = mulShift(ratio, mulShiftBy80)
 	}
 	if (absTick & 0x100) != 0 {
-
-		ratio = mulShift(ratio, "fcbe86c7900a88aedcffc83b479aa3a4")
+		ratio = mulShift(ratio, mulShiftBy100)
 	}
 	if (absTick & 0x200) != 0 {
-
-		ratio = mulShift(ratio, "f987a7253ac413176f2b074cf7815e54")
+		ratio = mulShift(ratio, mulShiftBy200)
 	}
 	if (absTick & 0x400) != 0 {
-
-		ratio = mulShift(ratio, "f3392b0822b70005940c7a398e4b70f3")
+		ratio = mulShift(ratio, mulShiftBy400)
 	}
 	if (absTick & 0x800) != 0 {
-
-		ratio = mulShift(ratio, "e7159475a2c29b7443b29c7fa6e889d9")
+		ratio = mulShift(ratio, mulShiftBy800)
 	}
 	if (absTick & 0x1000) != 0 {
-
-		ratio = mulShift(ratio, "d097f3bdfd2022b8845ad8f792aa5825")
+		ratio = mulShift(ratio, mulShiftBy1000)
 	}
 	if (absTick & 0x2000) != 0 {
-
-		ratio = mulShift(ratio, "a9f746462d870fdf8a65dc1f90e061e5")
+		ratio = mulShift(ratio, mulShiftBy2000)
 	}
 	if (absTick & 0x4000) != 0 {
-
-		ratio = mulShift(ratio, "70d869a156d2a1b890bb3df62baf32f7")
+		ratio = mulShift(ratio, mulShiftBy4000)
 	}
 	if (absTick & 0x8000) != 0 {
-
-		ratio = mulShift(ratio, "31be135f97d08fd981231505542fcfa6")
+		ratio = mulShift(ratio, mulShiftBy8000)
 	}
 	if (absTick & 0x10000) != 0 {
-
-		ratio = mulShift(ratio, "9aa508b5b7a84e1c677de54f3e99bc9")
+		ratio = mulShift(ratio, mulShiftBy10000)
 	}
 	if (absTick & 0x20000) != 0 {
-		ratio = mulShift(ratio, "5d6af8dedb81196699c329225ee604")
+		ratio = mulShift(ratio, mulShiftBy20000)
 	}
 	if (absTick & 0x40000) != 0 {
-
-		ratio = mulShift(ratio, "2216e584f5fa1ea926041bedfe98")
+		ratio = mulShift(ratio, mulShiftBy40000)
 	}
 	if (absTick & 0x80000) != 0 {
-
-		ratio = mulShift(ratio, "48a170391f7dc42444e8fa2")
+		ratio = mulShift(ratio, mulShiftBy80000)
 	}
+
 	if tick > 0 {
 		ratio = MaxUint256.Div(ratio).RoundDown(0)
 	}
+
 	_, remainder := ratio.QuoRem(Q32, 0)
 	remainder = remainder.RoundDown(0)
+
 	if remainder.GreaterThan(ZERO) {
 		return ratio.Div(Q32).RoundDown(0).Add(decimal.NewFromInt(1)), nil
 	} else {
